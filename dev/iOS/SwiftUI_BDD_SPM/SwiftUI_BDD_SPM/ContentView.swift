5ed4fd9722f3de35f0ec21496cc54dcb45950773
primecoder <ace.saengpetch@gmail.com>
2020-10-03 15:23:55 +1000

    Started new xcode prj

---
+++ dev/iOS/SwiftUI_BDD_SPM/SwiftUI_BDD_SPM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_BDD_SPM
+//
+//  Created by random on 3/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dev/iOS/SwiftUI_BDD_SPM/SwiftUI_BDD_SPM/SwiftUI_BDD_SPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BDD_SPMApp.swift
+//  SwiftUI_BDD_SPM
+//
+//  Created by random on 3/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BDD_SPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
